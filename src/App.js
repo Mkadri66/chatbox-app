2ecede9daf32761e216d4cd44c769d20071bd0e5
Mustapha Kadri <mustapha.kadri66@gmail.com>
2019-06-02 23:43:49 +0200

    feat(css) - add css animations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 import './App.css'
+import './animations.css'
 
 // Import components
 import Formulaire from './components/Formulaire'
@@ -8,12 +9,17 @@ import Message from './components/Message'
 // Import firebase
 import base from './base'
 
+// Import Animations
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+
 class App extends Component {
   state = {
     messages: {},
     // Pseudo récuperé depuis l'URL 
     pseudo: this.props.match.params.pseudo
   }
+
+  messagesRef = createRef()
 
   componentDidMount(){
     base.syncState('/',{
@@ -22,33 +28,58 @@ class App extends Component {
     })
   }
 
+  componentDidUpdate(){
+    const ref = this.messagesRef.current
+    //console.log(ref)
+    ref.scrollTop = ref.scrollHeight
+  }
+
   addMessage = (message) => {
     const messages = {...this.state.messages}
     // Ajout du message dans le state (à l'aide d'un timestamp )
     messages[`message-${Date.now()}`] = message
+    console.log(messages)
+    Object
+      .keys(messages)
+      .slice(0, -10)
+      .forEach(key => {
+        messages[key] = null
+      })
     this.setState({messages})
   }
+
+  isUser = (pseudo) => {
+    return pseudo === this.state.pseudo
+  }
+
+
   render () {
-   
     const messages = Object
     .keys(this.state.messages)
     .map(key => (
-      <Message 
-        key={key}
-        message={this.state.messages[key].message}
-        pseudo={this.state.messages[key].pseudo}
-      />
+      <CSSTransition
+        timeout={200}
+        classNames='fade'
+        key={key}>
+        <Message 
+          isUser={this.isUser}
+          message={this.state.messages[key].message}
+          pseudo={this.state.messages[key].pseudo}
+        />
+      </CSSTransition>
+
     ))
-
-    console.log(messages)
-
+    //console.log(messages)
     return (
-      <div className='box' >
+      <div className='box'>
         <div>
-          <div className="messages">
-            <div className="message">
+          <div 
+            className="messages" 
+            ref={this.messagesRef}
+            >
+            <TransitionGroup className="message">
               {messages}
-            </div>
+            </TransitionGroup>
           </div>
         </div>
         <Formulaire
